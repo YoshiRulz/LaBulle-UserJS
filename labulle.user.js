b074295c744dc35a215389eb4b4b49013d12321a
YoshiRulz <OSSYoshiRulz@gmail.com>
2018-02-02 04:01:15 +1100

    Upload v0.1

---
+++ labulle.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         LaBulle
+// @namespace    https://yoshirulz.github.io
+// @version      0.1
+// @description  Prevent (some of) others' stupidity from reaching and memetically infecting you.
+// @author       YoshiRulz
+// @match        */*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	"use strict";
+
+	window.runLaBulle = function() {
+		let temp = document.head.appendChild(document.createElement("style"));
+		temp.id = "labulle-stylesheet";
+		temp.innerHTML = ".labulle-corrected {\n\ttext-decoration: #BFFFDF underline wavy;\n\ttext-shadow: #DFFFEF 0 0 0.25em;\n}";
+		let replInRawDOM = function(e, find, findRaw, repl) {
+			e.innerHTML = e.innerHTML.replace(find,
+				"<span class='labulle-corrected' title='orig.:" + (findRaw.length > 73 ? "\n" : " ") + findRaw + "'>" + repl + "</span>");
+		};
+		//TODO decapitalization
+		temp = [ // RegEx special characters must be escaped
+			["could of", "could've"],
+			["could care less", "couldn't care less"]
+		];
+		for (let repl of temp) if (document.body.innerText.includes(repl[0])) {
+			let elems = [];
+			for (let tagName of ["a", "p", "yt-formatted-string"]) elems = elems.concat(Array.from(document.getElementsByTagName(tagName)));
+			let re = new RegExp(repl[0], "g");
+			for (let e of elems) if (e.innerHTML.includes(repl[0])) replInRawDOM(e, re, repl[0], repl[1]);
+		}
+	};
+
+	setTimeout(function() {
+		if (document.readyState === "complete") {
+			window.runLaBulle();
+		} else {
+			setTimeout(function() {
+				if (document.readyState === "complete") {
+					window.runLaBulle();
+				} else {
+					setTimeout(function() {
+						if (document.readyState === "complete") {
+							window.runLaBulle();
+						} else {
+							console.log("[LaBulle] Page not in \"complete\" ready state after ~25 seconds, aborting text correction for this page.");
+						}
+					}, 20000);
+				}
+			}, 2000);
+		}
+	}, 200);
+})();
